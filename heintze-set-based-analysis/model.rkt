7f5783e331edc8ba615be18db348a017d0a9b8f1
Ben Greenman <benjaminlgreenman@gmail.com>
2017-03-08 17:17:08 -0500

    [heintze] set-based operational semantics

--- heintze-set-based-analysis/model.rkt
+++ heintze-set-based-analysis/model.rkt
@@ -2,6 +2,8 @@
 
 (require
   racket/set
+  (only-in racket/random
+    random-ref)
   redex/reduction-semantics)
 
 (module+ test
@@ -14,16 +16,18 @@
  [e ::= ;; expressions
         x
         v
+        fx
         (c e ...)
         (λ x e)
         (e e)
-        (case e [(c e ...) ⇒ e] [x ⇒ e])
-        (fix x e)]
+        (case e [(c e ...) ⇒ e] [x ⇒ e])]
+ [fx ::= ;; fixpoint
+         (fix x e)]
  [E ::= ;; environments
         ((x bv) ...)]
  [bv ::= ;; binding value
          v
-         (fix x e)]
+         fx]
  [v ::= ;; values
         (c v ...)
         (E (λ x e))]
@@ -31,9 +35,15 @@
         cons
         A B C]
  [x ::= variable-not-otherwise-mentioned]
- #:binding-forms
-  (λ x e #:refers-to x)
-  (fix x e #:refers-to x))
+ ;#:binding-forms
+ ; (λ x e #:refers-to x)
+ ; (fix x e #:refers-to x)
+)
+(define e? (redex-match? FL e))
+(define E? (redex-match? FL E))
+(define bv? (redex-match? FL bv))
+(define v? (redex-match? FL v))
+(define fx? (redex-match? FL fx))
 
 (define-metafunction FL
   dom : E -> (x ...)
@@ -83,8 +93,8 @@
    --- Var-1
    (→ E x v)]
   [
-   (where (fix x e) #{ref E x})
-   (→ E (fix x e) v)
+   (where fx #{ref E x})
+   (→ E fx v)
    --- Var-2
    (→ E x v)]
   [
@@ -121,10 +131,6 @@
    (→ E (fix x e) v)])
 
 (module+ test
-  (define e? (redex-match? FL e))
-  (define E? (redex-match? FL E))
-  (define bv? (redex-match? FL bv))
-  (define v? (redex-match? FL v))
 
   (test-case "FL:redex-match"
     (check-true* e?
@@ -205,7 +211,53 @@
 
 (define-extended-language FLS FL
  [bv* ::= (bv ...)]
- [ξ ::= ((x bv*) ...)])
+ [ξ ::= ;; set environment
+        ((x bv*) ...)])
+(define bv*? (redex-match? FLS bv*))
+(define ξ? (redex-match? FLS ξ))
+
+(module+ test
+  (test-case "redex-match:FLS"
+    (check-true (bv? (term (B))))
+    (check-true (bv*? (term ((B)))))
+    (check-true (ξ? (term ((x ((B)))))))))
+
+(define-metafunction FLS
+  refs : ξ x -> bv*
+  [(refs ((x_0 bv*_0) ... (x bv*) (x_1 bv*_1) ...) x)
+   bv*])
+
+(module+ test
+  (test-case "refs"
+    (let ([ξ (term ((x ((A) (B))) (y ((B) (fix z (A)) (C)))))])
+      (check-equal?
+        (term (refs ,ξ x))
+        (term ((A) (B))))
+      (check-equal?
+        (term (refs ,ξ y))
+        (term ((B) (fix z (A)) (C)))))))
+
+;;; --- these 'refs' functions were an attempt to make
+;;      `(judgment-holds (~> ....))` terminate when infinite proofs exist
+;;; (define-metafunction FLS
+;;;   refs-v : ξ x -> any
+;;;   [(refs-v ξ x)
+;;;    v_0
+;;;    (where bv* ,(filter v? (term #{refs ξ x})))
+;;;    (side-condition (not (null? (term bv*))))
+;;;    (where v_0 ,(random-ref (term bv*)))]
+;;;   [(refs-v ξ x)
+;;;    FAIL:refs-v])
+;;; 
+;;; (define-metafunction FLS
+;;;   refs-fix : ξ x -> any
+;;;   [(refs-fix ξ x)
+;;;    fx_0
+;;;    (where bv* ,(filter fx? (term #{refs ξ x})))
+;;;    (side-condition (not (null? (term bv*))))
+;;;    (where fx_0 ,(random-ref (term bv*)))]
+;;;   [(refs-fix ξ x)
+;;;    FAIL:refs-fix])
 
 (define-judgment-form FLS
   #:mode (∈ I I)
@@ -219,7 +271,6 @@
    --- In-Var
    (∈ ((x_0 bv_0) (x_1 bv_1) ...) ξ)])
 
-
 (module+ test
   (test-case "∈"
     (check-true (judgment-holds (∈ () ())))
@@ -227,9 +278,113 @@
     (check-true (judgment-holds (∈ ((x (A))) ((x ((A) (B) (C)))))))
     (check-true (judgment-holds (∈ ((x (A)) (y (A))) ((y ((B) (A))) (x ((A) (B) (C)))))))
 
-    (check-false (judgment-holds (∈ ((x (A))) ())))
-  )
+    (check-false (judgment-holds (∈ ((x (A))) ())))))
+
+(define-judgment-form FLS
+  ;; vim seems to draw ~> faster than ⇝
+  #:mode (~> I I O)
+  #:contract (~> ξ e v)
+  [
+   (where (bv_0 ... v bv_1 ...) #{refs ξ x})
+   --- Var-1
+   (~> ξ x v)]
+  [
+   (where (bv_0 ... fx bv_1 ...) #{refs ξ x})
+   (~> ξ fx v)
+   --- Var-2
+   (~> ξ x v)]
+  [
+   (~> ξ e_1 (E (λ x e)))
+   (~> ξ e_2 v_2)
+   (~> ξ e v)
+   --- App
+   (~> ξ (e_1 e_2) v)]
+  [
+   (~> ξ e_1 v_1) ...
+   --- Const
+   (~> ξ (c e_1 ...) (c v_1 ...))]
+  [
+   (where E ()) ;; note: not part of Figure 2, but safe
+   (∈ E ξ)
+   --- Abs
+   (~> ξ (λ x e) (E (λ x e)))]
+  [
+   (~> ξ e_1 (c v_1 ...))
+   (~> ξ e_2 v)
+   --- Case-1
+   (~> ξ (case e_1 [(c x_1 ...) ⇒ e_2] [x_3 ⇒ e_3]) v)]
+  [
+   (~> ξ e_1 (c_2 v_2 ...))
+   (side-condition ,(not (equal? (term c) (term c_2))))
+   (~> ξ e_3 v)
+   --- Case-2
+   (~> ξ (case e_1 [(c x_1 ...) ⇒ e_2] [x_3 ⇒ e_3]) v)]
+  [
+   (~> ξ e v)
+   --- Fix
+   (~> ξ (fix x e) v)])
+
+;; "Observe that this second groups of rules will, in general, lead to an
+;;  unsound approximation. That is, certain set environments `ξ` will be such
+;;  that for some closed terms `e0`, `⊢ e0 → v` but `ξ ¬⊢ e0 ~> v`.
+(module+ test
+  (test-case "~>:unsound"
+    (let ([ξ0 (term ((x ((B)))))]
+          [e0 (term ((λ x x) (A)))]
+          [v (term (A))])
+      (check-equal? (term (eval ,e0)) v)
+      (check-false (judgment-holds (~> ,ξ0 ,e0 (A))))))
 )
+
+(module+ test
+  (test-case "~>:simple"
+    (check-true (judgment-holds
+      (~> ((x ((A) (B) (C))))
+          x
+          (A))))
+    (check-true (judgment-holds
+      (~> ((x ((A) (B))))
+          x
+          (A))))
+    #;(check-true (judgment-holds
+      (~> ((x ((A) (B) (fix y x))))
+          x
+          (A))))
+    (check-true (judgment-holds
+      (~> ((x ((A) (B) (fix z (λ r r)))) (y ((A) (C))) (r ((A) (C))))
+          (x y)
+          (A))))
+    (check-true (judgment-holds
+      (~> ((x ((A) (B))) (y ((A) (B))))
+          (cons x y)
+          (cons (B) (A)))))
+    (check-true (judgment-holds
+      (~> ((z ((A))))
+          (λ x (A))
+          (() (λ x (A))))))
+    (check-true (judgment-holds
+      (~> ((x ((A) (B) (C))) (y ((B) (C))) (z ((A) (B) (cons (A) (B)))))
+          (case (cons (A) (B))
+            [(cons x y) ⇒ x]
+            [z ⇒ (C)])
+          (A))))
+    (check-true (judgment-holds
+      (~> ((x ((A) (B) (C))) (y ((B) (C))) (z ((A) (B) (cons (A) (B)))))
+          (case (cons (A) (B))
+            [(B) ⇒ (A)]
+            [z ⇒ (C)])
+          (C))))
+    #;(check-true (judgment-holds
+      (~> ((n ((A) (B) (C)))
+           (fact ((fix fact (λ n (case n [(A) ⇒ (B)] [y ⇒ (fact (A))]))) (C))))
+          ((fix fact (λ n (case n [(A) ⇒ (B)] [y ⇒ (fact (A))]))) (C))
+          (B))))
+    (check-true (judgment-holds
+      (~> ((n ((A) (B) (C)))
+           (fact ((fix fact (λ n (case n [(A) ⇒ (B)] [y ⇒ (A)]))) (C))))
+          ((fix fact (λ n (case n [(A) ⇒ (B)] [y ⇒ (fact (A))]))) (C))
+          (A))))
+))
 
 ;; -----------------------------------------------------------------------------
 
