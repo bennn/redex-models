7ff574e8f713a86ee5193529230240dc142738af
Ben Greenman <benjaminlgreenman@gmail.com>
2017-03-09 00:24:09 -0500

    [heintze] \varepsion is safe wrt e

--- heintze-set-based-analysis/model.rkt
+++ heintze-set-based-analysis/model.rkt
@@ -2,12 +2,25 @@
 
 (require
   racket/set
+  (only-in racket/list
+    first
+    second
+    rest)
   (only-in racket/random
     random-ref)
-  redex/reduction-semantics)
+  redex/reduction-semantics
+  (for-syntax racket/base syntax/parse))
 
 (module+ test
   (require rackunit rackunit-abbrevs))
+
+(define *debug?* (make-parameter #f))
+
+(define (debug msg . arg*)
+  (when (*debug?*)
+    (printf "[DEBUG] ")
+    (apply printf msg arg*)
+    (newline)))
 
 ;; =============================================================================
 
@@ -34,6 +47,7 @@
  [c ::= ;; value constructor
         cons
         A B C]
+ [x* ::= (x ...)]
  [x ::= variable-not-otherwise-mentioned]
  ;#:binding-forms
  ; (λ x e #:refers-to x)
@@ -44,6 +58,7 @@
 (define bv? (redex-match? FL bv))
 (define v? (redex-match? FL v))
 (define fx? (redex-match? FL fx))
+(define x*? (redex-match? FL x*))
 
 (define-metafunction FL
   dom : E -> (x ...)
@@ -147,6 +162,11 @@
              [(cons x y) ⇒ x]
              [z ⇒ (C)]))])
 
+    (let ([env (term ((y (A))))])
+      (check-true (e? (term (,env (λ x y)))))
+      (check-false (e? env))
+      (check-true (E? env)))
+
     (check-true* E?
      [(term ())]
      [(term ((x1 (A)) (x2 (B))))]
@@ -223,9 +243,16 @@
     (check-true (ξ? (term ((x ((B)))))))))
 
 (define-metafunction FLS
-  refs : ξ x -> bv*
+  refs : ξ x -> any
   [(refs ((x_0 bv*_0) ... (x bv*) (x_1 bv*_1) ...) x)
-   bv*])
+   bv*]
+  [(refs ξ x)
+   ()])
+
+(define-metafunction FLS
+  doms : ξ -> any
+  [(doms ((x bv*) ...))
+   (x ...)])
 
 (module+ test
   (test-case "refs"
@@ -238,7 +265,7 @@
         (term ((B) (fix z (A)) (C)))))))
 
 ;;; --- these 'refs' functions were an attempt to make
-;;      `(judgment-holds (~> ....))` terminate when infinite proofs exist
+;;      `(judgment-holds (~~> ....))` terminate when infinite proofs exist
 ;;; (define-metafunction FLS
 ;;;   refs-v : ξ x -> any
 ;;;   [(refs-v ξ x)
@@ -281,110 +308,422 @@
     (check-false (judgment-holds (∈ ((x (A))) ())))))
 
 (define-judgment-form FLS
-  ;; vim seems to draw ~> faster than ⇝
-  #:mode (~> I I O)
-  #:contract (~> ξ e v)
+  ;; vim seems to draw ~~> faster than ⇝
+  #:mode (~~> I I O)
+  #:contract (~~> ξ e v)
   [
    (where (bv_0 ... v bv_1 ...) #{refs ξ x})
    --- Var-1
-   (~> ξ x v)]
+   (~~> ξ x v)]
   [
    (where (bv_0 ... fx bv_1 ...) #{refs ξ x})
-   (~> ξ fx v)
+   (~~> ξ fx v)
    --- Var-2
-   (~> ξ x v)]
+   (~~> ξ x v)]
   [
-   (~> ξ e_1 (E (λ x e)))
-   (~> ξ e_2 v_2)
-   (~> ξ e v)
+   (~~> ξ e_1 (E (λ x e)))
+   (~~> ξ e_2 v_2)
+   (~~> ξ e v)
    --- App
-   (~> ξ (e_1 e_2) v)]
+   (~~> ξ (e_1 e_2) v)]
   [
-   (~> ξ e_1 v_1) ...
+   (~~> ξ e_1 v_1) ...
    --- Const
-   (~> ξ (c e_1 ...) (c v_1 ...))]
+   (~~> ξ (c e_1 ...) (c v_1 ...))]
   [
    (where E ()) ;; note: not part of Figure 2, but safe
    (∈ E ξ)
    --- Abs
-   (~> ξ (λ x e) (E (λ x e)))]
+   (~~> ξ (λ x e) (E (λ x e)))]
   [
-   (~> ξ e_1 (c v_1 ...))
-   (~> ξ e_2 v)
+   (~~> ξ e_1 (c v_1 ...))
+   (~~> ξ e_2 v)
    --- Case-1
-   (~> ξ (case e_1 [(c x_1 ...) ⇒ e_2] [x_3 ⇒ e_3]) v)]
+   (~~> ξ (case e_1 [(c x_1 ...) ⇒ e_2] [x_3 ⇒ e_3]) v)]
   [
-   (~> ξ e_1 (c_2 v_2 ...))
+   (~~> ξ e_1 (c_2 v_2 ...))
    (side-condition ,(not (equal? (term c) (term c_2))))
-   (~> ξ e_3 v)
+   (~~> ξ e_3 v)
    --- Case-2
-   (~> ξ (case e_1 [(c x_1 ...) ⇒ e_2] [x_3 ⇒ e_3]) v)]
+   (~~> ξ (case e_1 [(c x_1 ...) ⇒ e_2] [x_3 ⇒ e_3]) v)]
   [
-   (~> ξ e v)
+   (~~> ξ e v)
    --- Fix
-   (~> ξ (fix x e) v)])
+   (~~> ξ (fix x e) v)])
 
 ;; "Observe that this second groups of rules will, in general, lead to an
 ;;  unsound approximation. That is, certain set environments `ξ` will be such
-;;  that for some closed terms `e0`, `⊢ e0 → v` but `ξ ¬⊢ e0 ~> v`.
+;;  that for some closed terms `e0`, `⊢ e0 → v` but `ξ ¬⊢ e0 ~~> v`.
 (module+ test
-  (test-case "~>:unsound"
+  (test-case "~~>:unsound"
     (let ([ξ0 (term ((x ((B)))))]
           [e0 (term ((λ x x) (A)))]
           [v (term (A))])
       (check-equal? (term (eval ,e0)) v)
-      (check-false (judgment-holds (~> ,ξ0 ,e0 (A))))))
+      (check-false (judgment-holds (~~> ,ξ0 ,e0 (A))))))
 )
 
 (module+ test
-  (test-case "~>:simple"
+  (test-case "~~>:simple"
     (check-true (judgment-holds
-      (~> ((x ((A) (B) (C))))
+      (~~> ((x ((A) (B) (C))))
           x
           (A))))
     (check-true (judgment-holds
-      (~> ((x ((A) (B))))
+      (~~> ((x ((A) (B))))
           x
           (A))))
     #;(check-true (judgment-holds
-      (~> ((x ((A) (B) (fix y x))))
+      (~~> ((x ((A) (B) (fix y x))))
           x
           (A))))
     (check-true (judgment-holds
-      (~> ((x ((A) (B) (fix z (λ r r)))) (y ((A) (C))) (r ((A) (C))))
+      (~~> ((x ((A) (B) (fix z (λ r r)))) (y ((A) (C))) (r ((A) (C))))
           (x y)
           (A))))
     (check-true (judgment-holds
-      (~> ((x ((A) (B))) (y ((A) (B))))
+      (~~> ((x ((A) (B))) (y ((A) (B))))
           (cons x y)
           (cons (B) (A)))))
     (check-true (judgment-holds
-      (~> ((z ((A))))
+      (~~> ((z ((A))))
           (λ x (A))
           (() (λ x (A))))))
     (check-true (judgment-holds
-      (~> ((x ((A) (B) (C))) (y ((B) (C))) (z ((A) (B) (cons (A) (B)))))
+      (~~> ((x ((A) (B) (C))) (y ((B) (C))) (z ((A) (B) (cons (A) (B)))))
           (case (cons (A) (B))
             [(cons x y) ⇒ x]
             [z ⇒ (C)])
           (A))))
     (check-true (judgment-holds
-      (~> ((x ((A) (B) (C))) (y ((B) (C))) (z ((A) (B) (cons (A) (B)))))
+      (~~> ((x ((A) (B) (C))) (y ((B) (C))) (z ((A) (B) (cons (A) (B)))))
           (case (cons (A) (B))
             [(B) ⇒ (A)]
             [z ⇒ (C)])
           (C))))
     #;(check-true (judgment-holds
-      (~> ((n ((A) (B) (C)))
+      (~~> ((n ((A) (B) (C)))
            (fact ((fix fact (λ n (case n [(A) ⇒ (B)] [y ⇒ (fact (A))]))) (C))))
           ((fix fact (λ n (case n [(A) ⇒ (B)] [y ⇒ (fact (A))]))) (C))
           (B))))
     (check-true (judgment-holds
-      (~> ((n ((A) (B) (C)))
+      (~~> ((n ((A) (B) (C)))
            (fact ((fix fact (λ n (case n [(A) ⇒ (B)] [y ⇒ (A)]))) (C))))
           ((fix fact (λ n (case n [(A) ⇒ (B)] [y ⇒ (fact (A))]))) (C))
           (A))))
+    (check-true (judgment-holds
+       (~~> ((d ((cons (A) (A)))))
+            (case (cons (A) (A))
+             [(A) ⇒ b]
+             [d ⇒ (C)])
+            (C))))
 ))
 
 ;; -----------------------------------------------------------------------------
+;; Section 2: "ξ is safe with respect to a closed term `e0` if, for every
+;;  derivation ending in `ξ ⊢ e0 ~~> v` the following four conditions are met:
+;;  1. every application of the rule APP is such that `v' ∈ ξ(x)`
+;;  2. every application of the rule CASE-1 is such that `vi ∈ ξ(xi)`
+;;  3. every application of the rule CASE-2 is such that `c'(v1 ... vn) ∈ ξ(x)`
+;;  4. every application of the rule FIX is such that `fix x e ∈ ξ(x)`
 
+(define-judgment-form FLS
+  #:mode (safe I I)
+  #:contract (safe ξ e)
+  [
+   (closed ξ e)
+   (side-condition ,(let ([d* (build-derivations (~~> ξ e v))])
+                      (and (not (null? d*))
+                           (app-safe? (find-derivations/name d* "App"))
+                           (case1-safe? (find-derivations/name d* "Case-1"))
+                           (case2-safe? (find-derivations/name d* "Case-2"))
+                           (fix-safe? (find-derivations/name d* "Fix")))))
+   --- IsSafe
+   (safe ξ e)])
+
+(define (find-derivations/name d* n)
+  (for/fold ([acc '()])
+            ([d (in-list d*)])
+    (append (if (string=? n (derivation-name d)) ;; if `-name` not string, we have a bigger problem
+              (cons d acc)
+              acc)
+            (find-derivations/name (derivation-subs d) n))))
+
+(module+ test
+  (test-case "redex-match:yolo"
+    (check-true (ξ? (term ((x ((B)))))))
+    (check-true (e? (term (λ x x))))
+    (check-true (e? (term (() (λ x x)))))))
+
+;; TODO depends on order of sub-derivations :/
+(define (app-safe? d*)
+  (for/and ([d (in-list d*)])
+    (define t (derivation-term d))
+    (define subs (map derivation-term (derivation-subs d)))
+    (debug "APP-SAFE term is ~a" t)
+    (redex-let FLS ([(~~> ξ (e_0 e_1) v_0) t])
+      (redex-let FLS ([(~~> ξ_3 e_0 (E (λ x e_3)))
+                       (first subs)]
+                      [(~~> ξ_6 e_1 v_1)
+                       (second subs)])
+        (member (term v_1) (term #{refs ξ x}))))))
+
+(define (case1-safe? d*)
+  (for/and ([d (in-list d*)])
+    (define t (derivation-term d))
+    (define subs (map derivation-term (derivation-subs d)))
+    (debug "CASE1-SAFE term is ~a" t)
+    (redex-let FLS ([(~~> ξ (case e_0 [(c x_1 ...) ⇒ e_1] [x_2 ⇒ e_2]) v_0) t])
+      (redex-let FLS ([(~~> ξ_3 e_0 (c v_1 ...))
+                       (first subs)])
+        (for/and ([x (in-list (term (x_1 ...)))]
+                  [v (in-list (term (v_1 ...)))])
+          (member v (term #{refs ξ ,x})))))))
+
+(define (case2-safe? d*)
+  (for/and ([d (in-list d*)])
+    (define t (derivation-term d))
+    (define subs (map derivation-term (derivation-subs d)))
+    (debug "CASE2-SAFE term is ~a" t)
+    (redex-let FLS ([(~~> ξ (case e_0 [(c_0 x_1 ...) ⇒ e_1] [x_2 ⇒ e_2]) v_0) t])
+      (redex-let FLS ([(~~> ξ_3 e_0 (c v_1 ...))
+                       (first subs)])
+        (and (member (term (c v_1 ...)) (term #{refs ξ x_2})) #t)))))
+
+(define (fix-safe? d*)
+  (for/and ([d (in-list d*)])
+    (define t (derivation-term d))
+    (define subs (map derivation-term (derivation-subs d)))
+    (debug "FIX-SAFE term is ~a" t)
+    (redex-let FLS ([(~~> ξ (fix x e) v_0) t])
+      (and (member (term (fix x e)) (term #{refs ξ x})) #t))))
+
+(define-judgment-form FLS
+  #:mode (closed I I)
+  #:contract (closed ξ e)
+  [
+   (fvs e x*)
+   (where () ,(set-subtract (term x*) (term #{doms ξ})))
+   ---
+   (closed ξ e)])
+
+(define-judgment-form FLS
+  #:mode (fvs I O)
+  #:contract (fvs e (x ...))
+  [
+   (where x* (x))
+   --- FVS-Var
+   (fvs x x*)]
+  [
+   (fvs e x*_0) ...
+   (where x* ,(if (null? (term (e ...)))
+                (term ())
+                (apply set-union (term (x*_0 ...)))))
+   --- FVS-c
+   (fvs (c e ...) x*)]
+  [
+   (fvs (λ x e) x*_0)
+   (where x* ,(set-subtract (term x*_0) (term #{dom E})))
+   --- FVS-Closure
+   (fvs (E (λ x e)) x*)]
+  [
+   (fvs e x*_0)
+   (where x* ,(set-remove (term x*_0) (term x)))
+   --- FVS-Fix
+   (fvs (fix x e) x*)]
+  [
+   (fvs e x*_0)
+   (where x* ,(set-remove (term x*_0) (term x)))
+   --- FVS-λ
+   (fvs (λ x e) x*)]
+  [
+   (fvs e_0 x*_0)
+   (fvs e_1 x*_1)
+   (where x* ,(set-union (term x*_0) (term x*_1)))
+   --- FVS-App
+   (fvs (e_0 e_1) x*)]
+  [
+   (fvs e_0 x*_0)
+   (fvs e_1 x*_1)
+   (fvs e_2 x*_2)
+   (where x* ,(set-union (term x*_0)
+                         (set-subtract (term x*_1) (term (x_1 ...)))
+                         (set-remove (term x*_2) (term x_2))))
+   --- FVS-Case
+   (fvs (case e_0 [(c x_1 ...) ⇒ e_1] [x_2 ⇒ e_2]) x*)])
+
+(define-metafunction FLS
+  fvs# : e -> x*
+  [(fvs# e)
+   x*
+   (judgment-holds (fvs e x*_0))
+   (where x* ,(sort (term x*_0) symbol<?))])
+
+(module+ test
+  (test-case "fvs"
+    (check-equal?
+      (term #{fvs# x})
+      (term (x)))
+    (check-equal?
+      (term #{fvs# (cons x1 x2)})
+      (term (x1 x2)))
+    (check-equal?
+      (term #{fvs# (cons (A) (B))})
+      (term ()))
+    (check-equal?
+      (term #{fvs# (A)})
+      '())
+    (check-equal?
+      (term #{fvs# (() (λ x x))})
+      '())
+    (check-equal?
+      (term #{fvs# (() (λ x y))})
+      '(y))
+    (check-equal?
+      (term #{fvs# (((y (A))) (λ x y))})
+      '())
+    (check-equal?
+      (term #{fvs# (fix z (λ n (A)))})
+      '())
+    (check-equal?
+      (term #{fvs# (fix z (λ n (A)))})
+      '())
+    (check-equal?
+      (term #{fvs# (fix z (λ n (z (A))))})
+      '())
+    (check-equal?
+      (term #{fvs# (λ x x)})
+      '())
+    (check-equal?
+      (term #{fvs# ((A) (A))})
+      '())
+    (check-equal?
+      (term #{fvs# (x y)})
+      '(x y))
+    (check-equal?
+      (term #{fvs# (case a [(cons b c) ⇒ d] [e ⇒ f])})
+      '(a d f))
+    (check-equal?
+      (term #{fvs# (case a [(cons b c) ⇒ b] [e ⇒ e])})
+      '(a))
+    (check-equal?
+      (term #{fvs# (case (A) [(cons b c) ⇒ b] [e ⇒ e])})
+      '())
+  )
+
+  (test-case "closed"
+    (check-false (judgment-holds (closed () x)))
+    (check-false (judgment-holds (closed () (cons x1 x2))))
+    (check-true (judgment-holds (closed () (cons (A) (B)))))
+    (check-true (judgment-holds (closed () (A))))
+    (check-true (judgment-holds (closed () (() (λ x x)))))
+    (check-false (judgment-holds (closed () (() (λ x y)))))
+    (check-true (judgment-holds (closed () (((y (A))) (λ x y)))))
+    (check-true (judgment-holds (closed () (fix z (λ n (A))))))
+    (check-true (judgment-holds (closed () (fix z (λ n (A))))))
+    (check-true (judgment-holds (closed () (fix z (λ n (z (A)))))))
+    (check-true (judgment-holds (closed () (λ x x))))
+    (check-true (judgment-holds (closed () ((A) (A)))))
+    (check-false (judgment-holds (closed () (x y))))
+    (check-false (judgment-holds (closed () (case a [(cons b c) ⇒ d] [e ⇒ f]))))
+    (check-false (judgment-holds (closed () (case a [(cons b c) ⇒ b] [e ⇒ e]))))
+    (check-true (judgment-holds (closed () (case (A) [(cons b c) ⇒ b] [e ⇒ e]))))
+    (check-false (judgment-holds (closed ()
+      (case (cons (A) (A))
+       [(A) ⇒ b]
+       [d ⇒ (C)]))))
+    (check-false (judgment-holds
+      (closed ()
+              (case (cons (A) (A))
+               [(A) ⇒ b]
+               [d ⇒ (C)]))))
+
+    (check-true (judgment-holds
+      (closed ((b ((A))))
+              (case (cons (A) (A))
+               [(A) ⇒ b]
+               [d ⇒ (C)]))))
+  )
+
+  (test-case "pre-safe"
+    (check-equal? ;; sanity check
+      (length (build-derivations (~~> ((x ((A) (B) (C)))) x v)))
+      3))
+
+  (test-case "app-safe"
+    (check-false (judgment-holds
+      (safe () ((λ x x) (A)))))
+    (check-false (judgment-holds
+      (safe ((x ((B)))) ((λ x x) (A)))))
+    (check-true (judgment-holds
+      (safe ((x ((A)))) ((λ x x) (A)))))
+    (check-true (judgment-holds
+      (safe ((x ((C) (A) (B)))) ((λ x x) (A))))))
+
+  (test-case "case1-safe"
+    (check-false (judgment-holds
+      (safe ((x ((A))))
+            (case (cons (A) (A))
+             [(cons a b) ⇒ b]
+             [d ⇒ (C)]))))
+    (check-true (judgment-holds
+      (safe ((a ((A))) (b ((A))))
+            (case (cons (A) (A))
+             [(cons a b) ⇒ b]
+             [d ⇒ (C)]))))
+    (check-true (judgment-holds
+      (safe ((a ((B) (A))) (b ((cons (B) (B)) (A))))
+            (case (cons (A) (A))
+             [(cons a b) ⇒ b]
+             [d ⇒ (C)]))))
+    (check-false (judgment-holds
+      (safe ((a ((A))) (b ((B))))
+            (case (cons (A) (A))
+             [(cons a b) ⇒ b]
+             [d ⇒ (C)]))))
+  )
+
+  (test-case "case2-safe"
+    (check-false (judgment-holds
+      (safe ((x ((A))))
+            (case (cons (A) (A))
+             [(A) ⇒ b]
+             [d ⇒ (C)]))))
+    (check-true (judgment-holds
+      (safe ((d ((cons (A) (A)))))
+            (case (cons (A) (A))
+             [(A) ⇒ (B)]
+             [d ⇒ (C)]))))
+    (check-true (judgment-holds
+      (safe ((a ((A))) (b ((A))) (d ((A) (cons (A) (A)))))
+            (case (cons (A) (A))
+             [(A) ⇒ b]
+             [d ⇒ (C)]))))
+    (check-false (judgment-holds
+      (safe ((d ((A))))
+            (case (cons (A) (A))
+             [(A) ⇒ (B)]
+             [d ⇒ (C)]))))
+  )
+
+  (test-case "fix-safe"
+    (check-false (judgment-holds
+      (safe () (fix x (A)))))
+    (check-true (judgment-holds
+      (safe ((x ((fix x (A))))) (fix x (A)))))
+    (check-true (judgment-holds
+      (safe ((x ((A) (B) (C) (fix x (A))))) (fix x (A)))))
+    (check-true (judgment-holds
+      (safe ((x ((fix x (A)))))
+            (case (A) [(A) ⇒ (fix x (A))]
+                      [z ⇒ (fix x (A))]))))
+    (check-true (judgment-holds
+      (safe ((x ((fix x (A)))) (y ((fix y (λ n (B))))))
+            (case (A) [(A) ⇒ (fix y (λ n (B)))]
+                      [z ⇒ (fix x (A))]))))
+    (check-false (judgment-holds
+      (safe ((x ((fix x (A)))) (y ((B))))
+            (case (A) [(A) ⇒ (fix y (λ n (B)))]
+                      [z ⇒ (fix x (A))]))))
+  )
+)
