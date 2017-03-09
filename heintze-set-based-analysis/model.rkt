1a9103753786d52b4bb27777432f7de95d2eaf5e
Ben Greenman <benjaminlgreenman@gmail.com>
2017-03-09 17:10:33 -0500

    [heintze] checkpoint: set constraints and interpretation

--- heintze-set-based-analysis/model.rkt
+++ heintze-set-based-analysis/model.rkt
@@ -44,21 +44,66 @@
  [v ::= ;; values
         (c v ...)
         (E (λ x e))]
+ [v- ::= ;; norm'd values
+         (c v- ...)
+         (λ x e)]
  [c ::= ;; value constructor
         cons
         A B C]
  [x* ::= (x ...)]
- [x ::= variable-not-otherwise-mentioned]
+ ;; -----------------------------------------------------------------------------
+ ;; Section 2:
+ [bv* ::= (bv ...)]
+ [ξ ::= ;; set environment
+        ((x bv*) ...)]
+ ;; -----------------------------------------------------------------------------
+ ;; Section 3: Set Constraints
+ [setvar ::= ;; assuming: setvar for each program variable AND setvar for each λ range
+             X
+             (ran (λ x e))]
+ [se ::= ;; set expression
+         setvar
+         (λ x e)
+         (c se ...)
+         (apply se se)
+         (case se [(c X ...) ⇒ se] [X ⇒ se])
+         (ifnotempty se se)]
+ [constr ::= ;; set constraint
+             (se ⊆ X)]
+ [conj ::= ;; conjunction of set constraints
+            (constr ...)]
+ [sc-value ::= ;; set constraint value, equal to v-
+               (λ x e)
+               (c sc-value ...)]
+ [sc-value* ::= ;; set of sc-value
+                (sc-value ...)]
+ [IN ::= ;; interpretation (map from set-variables to set-values)
+         ((setvar sc-value*) ...)]
+ ;; -----------------------------------------------------------------------------
+ [x X ::= ;; lowercase = program variable, uppercase = set variable
+          variable-not-otherwise-mentioned]
  ;#:binding-forms
  ; (λ x e #:refers-to x)
  ; (fix x e #:refers-to x)
 )
 (define e? (redex-match? FL e))
+(define c? (redex-match? FL c))
 (define E? (redex-match? FL E))
 (define bv? (redex-match? FL bv))
 (define v? (redex-match? FL v))
+(define v-? (redex-match? FL v-))
 (define fx? (redex-match? FL fx))
 (define x*? (redex-match? FL x*))
+(define bv*? (redex-match? FL bv*))
+(define ξ? (redex-match? FL ξ))
+(define setvar? (redex-match? FL setvar))
+(define se? (redex-match? FL se))
+(define constr? (redex-match? FL constr))
+(define conj? (redex-match? FL conj))
+(define sc-value? (redex-match? FL sc-value))
+
+(define (λ? t) (and (pair? t) (eq? 'λ (car t))))
+(define (constant? t) (and (pair? t) (c? (car t))))
 
 (define-metafunction FL
   dom : E -> (x ...)
@@ -180,7 +225,11 @@
     (check-true* v?
      [(term (B))]
      [(term (cons (A) (B)))]
+     [(term [((x (A))) (λ y x)])]
+     [(term (cons (A) [((x (A))) (λ y x)]))]
      [(term (() (λ n n)))]))
+
+  (check-true (v-? (term (cons (A) (λ y x)))))
 
   (test-case "dom"
     (check-apply* (λ (x) (term (dom ,x)))
@@ -229,27 +278,27 @@
 ;; Section 2: "We now modify the operational semantics so that dependencies
 ;;  between variables are ignored"
 
-(define-extended-language FLS FL
- [bv* ::= (bv ...)]
- [ξ ::= ;; set environment
-        ((x bv*) ...)])
-(define bv*? (redex-match? FLS bv*))
-(define ξ? (redex-match? FLS ξ))
-
 (module+ test
-  (test-case "redex-match:FLS"
+  (test-case "redex-match:FL"
     (check-true (bv? (term (B))))
     (check-true (bv*? (term ((B)))))
     (check-true (ξ? (term ((x ((B)))))))))
 
-(define-metafunction FLS
+(define-metafunction FL
   refs : ξ x -> any
   [(refs ((x_0 bv*_0) ... (x bv*) (x_1 bv*_1) ...) x)
    bv*]
   [(refs ξ x)
    ()])
 
-(define-metafunction FLS
+(define-metafunction FL
+  irefs : IN setvar -> any
+  [(irefs ((setvar_0 sc-value*_0) ... (setvar sc-value*) (setvar_1 sc-value*_1) ...) setvar)
+   sc-value*]
+  [(irefs IN setvar)
+   ()])
+
+(define-metafunction FL
   doms : ξ -> any
   [(doms ((x bv*) ...))
    (x ...)])
@@ -266,7 +315,7 @@
 
 ;;; --- these 'refs' functions were an attempt to make
 ;;      `(judgment-holds (~~> ....))` terminate when infinite proofs exist
-;;; (define-metafunction FLS
+;;; (define-metafunction FL
 ;;;   refs-v : ξ x -> any
 ;;;   [(refs-v ξ x)
 ;;;    v_0
@@ -276,7 +325,7 @@
 ;;;   [(refs-v ξ x)
 ;;;    FAIL:refs-v])
 ;;; 
-;;; (define-metafunction FLS
+;;; (define-metafunction FL
 ;;;   refs-fix : ξ x -> any
 ;;;   [(refs-fix ξ x)
 ;;;    fx_0
@@ -286,7 +335,7 @@
 ;;;   [(refs-fix ξ x)
 ;;;    FAIL:refs-fix])
 
-(define-judgment-form FLS
+(define-judgment-form FL
   #:mode (∈ I I)
   #:contract (∈ E ξ)
   [
@@ -307,7 +356,7 @@
 
     (check-false (judgment-holds (∈ ((x (A))) ())))))
 
-(define-judgment-form FLS
+(define-judgment-form FL
   ;; vim seems to draw ~~> faster than ⇝
   #:mode (~~> I I O)
   #:contract (~~> ξ e v)
@@ -427,7 +476,7 @@
 ;;  3. every application of the rule CASE-2 is such that `c'(v1 ... vn) ∈ ξ(x)`
 ;;  4. every application of the rule FIX is such that `fix x e ∈ ξ(x)`
 
-(define-judgment-form FLS
+(define-judgment-form FL
   #:mode (safe I I)
   #:contract (safe ξ e)
   [
@@ -461,8 +510,8 @@
     (define t (derivation-term d))
     (define subs (map derivation-term (derivation-subs d)))
     (debug "APP-SAFE term is ~a" t)
-    (redex-let FLS ([(~~> ξ (e_0 e_1) v_0) t])
-      (redex-let FLS ([(~~> ξ_3 e_0 (E (λ x e_3)))
+    (redex-let FL ([(~~> ξ (e_0 e_1) v_0) t])
+      (redex-let FL ([(~~> ξ_3 e_0 (E (λ x e_3)))
                        (first subs)]
                       [(~~> ξ_6 e_1 v_1)
                        (second subs)])
@@ -473,8 +522,8 @@
     (define t (derivation-term d))
     (define subs (map derivation-term (derivation-subs d)))
     (debug "CASE1-SAFE term is ~a" t)
-    (redex-let FLS ([(~~> ξ (case e_0 [(c x_1 ...) ⇒ e_1] [x_2 ⇒ e_2]) v_0) t])
-      (redex-let FLS ([(~~> ξ_3 e_0 (c v_1 ...))
+    (redex-let FL ([(~~> ξ (case e_0 [(c x_1 ...) ⇒ e_1] [x_2 ⇒ e_2]) v_0) t])
+      (redex-let FL ([(~~> ξ_3 e_0 (c v_1 ...))
                        (first subs)])
         (for/and ([x (in-list (term (x_1 ...)))]
                   [v (in-list (term (v_1 ...)))])
@@ -485,8 +534,8 @@
     (define t (derivation-term d))
     (define subs (map derivation-term (derivation-subs d)))
     (debug "CASE2-SAFE term is ~a" t)
-    (redex-let FLS ([(~~> ξ (case e_0 [(c_0 x_1 ...) ⇒ e_1] [x_2 ⇒ e_2]) v_0) t])
-      (redex-let FLS ([(~~> ξ_3 e_0 (c v_1 ...))
+    (redex-let FL ([(~~> ξ (case e_0 [(c_0 x_1 ...) ⇒ e_1] [x_2 ⇒ e_2]) v_0) t])
+      (redex-let FL ([(~~> ξ_3 e_0 (c v_1 ...))
                        (first subs)])
         (and (member (term (c v_1 ...)) (term #{refs ξ x_2})) #t)))))
 
@@ -495,10 +544,10 @@
     (define t (derivation-term d))
     (define subs (map derivation-term (derivation-subs d)))
     (debug "FIX-SAFE term is ~a" t)
-    (redex-let FLS ([(~~> ξ (fix x e) v_0) t])
+    (redex-let FL ([(~~> ξ (fix x e) v_0) t])
       (and (member (term (fix x e)) (term #{refs ξ x})) #t))))
 
-(define-judgment-form FLS
+(define-judgment-form FL
   #:mode (closed I I)
   #:contract (closed ξ e)
   [
@@ -507,7 +556,7 @@
    ---
    (closed ξ e)])
 
-(define-judgment-form FLS
+(define-judgment-form FL
   #:mode (fvs I O)
   #:contract (fvs e (x ...))
   [
@@ -552,7 +601,7 @@
    --- FVS-Case
    (fvs (case e_0 [(c x_1 ...) ⇒ e_1] [x_2 ⇒ e_2]) x*)])
 
-(define-metafunction FLS
+(define-metafunction FL
   fvs# : e -> x*
   [(fvs# e)
    x*
@@ -740,7 +789,7 @@
 ;; Section 2: Theorem 1: Soundness: if ξ is safe wrt a closed term e0,
 ;; then  `⊢ e0 → v` implies `ξ ⊢ e0 ~~> v`
 
-(define-metafunction FLS
+(define-metafunction FL
   check-theorem-1 : ξ e -> boolean
   [(check-theorem-1 ξ e)
    #true
@@ -821,7 +870,7 @@
 ;;  `e0`, then so is `ξ_1 ∩ ξ_2`. Moreover `ξ_1 ∩ ξ_2 ⊢ e0 ~~> v` implies
 ;;  `ξ_1 ⊢ e_0 ~~> v` and `ξ_2 ⊢ e0 ~~> v`.
 
-(define-metafunction FLS
+(define-metafunction FL
   check-proposition-1 : ξ ξ e -> boolean
   [(check-proposition-1 ξ_1 ξ_2 e_0)
    #true
@@ -860,7 +909,7 @@
    #f
    ])
 
-(define-metafunction FLS
+(define-metafunction FL
   ∩ : ξ ξ -> any
   [(∩ ξ_1 ())
    ξ_1]
@@ -892,3 +941,151 @@
 ;;
 ;; (Cannot program this because no way to pick `ξ_0`)
 
+;; =============================================================================
+;; Section 3: Main Result: Algorithm for computing `sba(e_0)`
+;; - first we construct set constraints corresponding to the input term e0
+;; - the second part of the algorithm is a simplification procedure for set constraints
+
+;; The environment part of closures in `sba(e0)` is essentially redundant
+;; ... define an operator ||v|| on values `v` which forgets the environment part
+;; of closures
+(define-judgment-form FL
+  #:mode (norm I O)
+  #:contract (norm v v-)
+  [
+   (norm v v-) ...
+   --- Norm-C
+   (norm (c v ...) (c v- ...))]
+  [
+   --- Norm-λ
+   (norm (E (λ x e)) (λ x e))])
+
+(module+ test
+  (test-case "norm"
+    (check-true (judgment-holds
+      (norm (() (λ x (A))) (λ x (A)))))
+    (check-true (judgment-holds
+      (norm [((x (A)) (y (B))) (λ z ((λ a x) y))] (λ z ((λ a x) y)))))
+    (check-true (judgment-holds
+      (norm (cons (A) [((x (A))) (λ y x)]) (cons (A) (λ y x)))))))
+
+;; The algorithm computes a representation of the set ||sba(e0)||
+
+;; -----------------------------------------------------------------------------
+;; 
+
+(define-metafunction FL
+  var->setvar : x -> x
+  [(var->setvar x)
+   ,(string->symbol (string-upcase (symbol->string (term x))))
+   (side-condition (lowercase? (term x)))]
+  [(var->setvar x)
+   ,(raise-user-error 'var->setvar "variable ~a is a set variable (uppercase)" (term x))
+   (side-condition (not (lowercase? (term x))))])
+
+(define (lowercase? sym)
+  (define str (symbol->string sym))
+  (string=? str (string-downcase str)))
+
+(module+ test
+  (test-case "var->setvar"
+    (check-apply* (λ (t) (term #{var->setvar ,t}))
+     [(term x)
+      ==> (term X)]
+     [(term z)
+      ==> (term Z)]
+     [(term gigawatt)
+      ==> (term GIGAWATT)])
+
+    (check-exn exn:fail:user?
+      (λ () (term #{var->setvar X}))))
+
+  (test-case "lowercase?"
+    (check-apply* lowercase?
+     ['a
+      ==> #true]
+     ['A
+      ==> #false]
+     ['aPPle
+      ==> #false]
+     ['apple
+      ==> #true]))
+)
+
+
+(define-judgment-form FL
+  #:mode (interpretation I I O)
+  #:contract (interpretation IN se sc-value*)
+  [
+   (where sc-value* #{irefs IN setvar})
+   --- I-var
+   (interpretation IN setvar sc-value*)]
+  [
+   (where (sc-value*_0 ...) (#{irefs IN se_0} ...))
+   (where sc-value* ,(apply set-union (term (sc-value*_0 ...))))
+   --- I-C
+   (interpretation IN (c se_0 ...) sc-value*)]
+  [
+   --- I-λ
+   (interpretation IN (λ x e) ((λ x e)))]
+  [
+   (interpretation IN se_0 ())
+   --- I-ifnonempty-1
+   (interpretation IN (ifnonempty se_0 se_1) ())]
+  [
+   (interpretation IN se_0 sc-value*_0)
+   (side-condition ,(not (null? (term sc-value*_0))))
+   (interpretation IN se_1 sc-value*)
+   --- I-ifnonempty-2
+   (interpretation IN (ifnonempty se_0 se_1) sc-value*)]
+  [
+   (interpretation IN se_0 sc-value*_0)
+   (where (sc-value_0 ...) ,(filter λ? (term sc-value*_0)))
+   (interpretation IN se_1 sc-value*_1)
+   (side-condition ,(not (null? (term sc-value*_1))))
+   (interpretation IN (ran sc-value_0) sc-value*_2) ...
+   ;; damn
+   (side-condition ,(for/and ([x (in-list (map cadr (term (sc-value_0 ...))))])
+                      (subset? (term sc-value*_1) (term #{irefs IN #{var->setvar ,x}}))))
+   (where sc-value* ,(apply set-union (term (sc-value*_2 ...))))
+   --- I-apply
+   (interpretation IN (apply se_0 se_1) sc-value*)]
+  [
+   (interpretation IN se_0 sc-value*_0)
+   (interpretation IN se_1 sc-value*_3)
+   (interpretation IN se_2 sc-value*_4)
+   (where sc-value*_5 ,(filter (λ (t) (and (constant? t) (eq? (term c) (car t))))
+                               (term sc-value*_0)))
+   (where sc-value*_6 ,(filter (λ (t) (and (constant? t) (not (eq? (term c) (car t)))))
+                               (term sc-value*_0)))
+   (where sc-value*_1 ,(if (null? (term sc-value*_5)) (term ()) (term sc-value*_3)))
+   (where sc-value*_2 ,(if (null? (term sc-value*_6)) (term ()) (term sc-value*_4)))
+   (where sc-value* ,(set-union (term sc-value*_1) (term sc-value*_2)))
+   (side-condition ,(and
+                      (for/and ([cnst (in-list (term sc-value*_5))])
+                        (for/and ([v (in-list (cdr cnst))]
+                                  [x (in-list (term (X_1 ...)))])
+                          (member v (term #{irefs IN ,x}))))
+                      (let ([scv* (term #{irefs IN X_2})])
+                        (for/and ([cnst (in-list (term sc-value*_6))])
+                          (member cnst scv*)))))
+   --- I-case
+   (interpretation IN (case se_0 [(c X_1 ...) ⇒ se_1] [X_2 ⇒ se_2]) sc-value*)])
+
+;; An interpretation I is a model of a conjunction of constraints `C` if, for
+;; each constraint `se ⊆ X` it is the case that `I(se)` is defined and
+;; `I(se) ⊆ I(X)`
+(define-judgment-form FL
+  #:mode (⊨ I I)
+  #:contract (⊨ IN C)
+  [
+   --- Models-Empty
+   (⊨ IN ())]
+  [
+   (interpretation IN se sc-value*_0)
+   (interpretation IN X sc-value*_1)
+   (side-condition ,(and (not (null? (term sc-value*_0)))
+                         (subset? (term sc-value*_0) (term sc-value*_1))))
+   (⊨ IN (constr_1 ...))
+   --- Models-Cons
+   (⊨ IN ((se ⊆ X) constr_1 ...))])
