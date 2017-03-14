a60c9c32b8319a41abec6c4d6425f4d2f9dc85c7
Ben Greenman <benjaminlgreenman@gmail.com>
2017-03-14 16:28:15 -0400

    [flanagan] init

---
+++ flanagan-set-based-analysis/README.md
@@ -0,0 +1,5 @@
+flanagan-set-based-analysis
+===
+
+_Effective Static Debugging via Componential Set-Based Analysis_
+Cormac Flanagan [pdf](./src/f-diss-1997.pdf)
---
+++ flanagan-set-based-analysis/model.rkt
@@ -0,0 +1,12 @@
+#lang mf-apply racket/base
+
+(require
+  racket/set
+  redex/reduction-semantics)
+
+(module+ test
+  (require rackunit rackunit-abbrevs))
+
+;; =============================================================================
+
+
